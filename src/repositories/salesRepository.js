bb04c2de508f2ead11d301d03699211ec6bbd779
Felipe <mr.tvz569@gmail.com>
2022-07-31 18:03:53 -0300

    added sales ropository functions

---
+++ src/repositories/salesRepository.js
@@ -0,0 +1,24 @@
+import { prisma } from "../db";
+
+async function createSale(data) {
+  await prisma.sales.create({
+    data: data
+  })
+}
+
+async function getNumberOfSoldProducts(productId) {
+  return prisma.sales.groupBy({
+    by: ['productId'],
+    where: {
+      productId: productId
+    },
+    _sum: {
+      quantity: true
+    }
+  })
+}
+
+export default {
+  createSale,
+  getNumberOfSoldProducts
+}
