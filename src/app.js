bcbd5f2f9fd548f2cc1c4e4d9af88d70c9588776
Felipe <mr.tvz569@gmail.com>
2022-08-01 15:58:35 -0300

    integrate routers with app

--- src/app.js
+++ src/app.js
@@ -2,13 +2,25 @@ import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import handleApplicationErrors from "./middlewares/errorHandlingMiddleware.js";
+import branchRouter from "./routers/branchRouter.js";
+import companyRouter from "./routers/companyRouter.js";
+import employeeRouter from "./routers/employeeRouter.js";
 import router from "./routers/index.js";
+import salesRouter from "./routers/salesRouter.js";
+import stockRouter from "./routers/stockRouter.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(router)
-app.use(handleApplicationErrors)
+app
+  .use(cors())
+  .use(express.json())
+  .use('/users', userRouter)
+  .use('/company', companyRouter)
+  .use('/branch', branchRouter)
+  .use('/employee', employeeRouter)
+  .use('/sales', salesRouter)
+  .use('/stock', stockRouter)
+  .use(handleApplicationErrors)
 
 
 export default app;
